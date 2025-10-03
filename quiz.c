69d8d59711b70eaee03105b92be81709f626643a
Keshvi <keshvisolanki50@gmail.com>
2025-10-02 21:42:16 -0400

    skeleton code

---
+++ quiz.c
@@ -0,0 +1,88 @@
+#include <stdio.h>  // printing
+#include <string.h> // comparing string
+#include <stdlib.h> // exit and other things
+/**
+ * if quiz -q --> all question
+ * if quiz -q w/ # --> each question
+ * if quiz -a # --> each answer
+ * if quiz --help --> help info
+ */
+int main(int argc, char *argv[])
+{
+  // argc - how many arguments
+  // argv - arguments
+
+  char *question[] = {
+      "Name?", "age?", "year?"};
+
+  int number_questions = 3;
+  char *answer[] = {
+      "keshvi", "12", "1997"};
+
+
+  if (strcmp(argv[1], "-q") == 0)
+  {
+    if (argc == 2)
+    {
+      printf("all questions");
+    }
+    else if (strcmp(argv[2], "1")==0)
+    {
+      printf("question 1");
+    }
+    else if (strcmp(argv[2], "2")== 0)
+    {
+      printf("question 2");
+    }
+    else if (strcmp(argv[2], "3" )== 0)
+    {
+      printf("question 3");
+    }
+    else
+    {
+      printf("invalid number");
+    }
+  }
+
+  // ANSWERS
+  else if (strcmp(argv[1], "-a") == 0)
+  {
+
+    if (argc == 2)
+    {
+      printf("need to enter ans as well");
+    }
+    else
+    {
+      if (strcmp(argv[2], "1")==0 && strcmp(argv[3], answer[0]) == 0)
+      {
+        printf("right");
+      }
+      else{
+        printf("wrong");
+      }
+     if (strcmp(argv[2], "2") ==0&& strcmp(argv[3], answer[1]) == 0)
+      {
+        printf("right");
+      }
+      else{
+        printf("wrong");
+      }
+      if (strcmp(argv[2], "3")==0 && strcmp(argv[3], answer[2]) == 0)
+      {
+        printf("right");
+      }
+      else{
+        printf("wrong");
+      }
+    }}
+// HELP
+  else if (strcmp(argv[1], "--help") == 0)
+  {
+    printf("help instructions");
+  }
+  else
+  {
+    printf("enter valid ans");
+  }
+}
